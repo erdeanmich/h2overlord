05d741f28caccd29b64a983524ea5291055c30f1
erdeanmich <erdeanlinda@gmail.com>
2025-04-02 19:58:06 +0200

    Add test script

---
+++ h2overlord-python/src/h2overlord_python/test.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+import gpiozero
+
+RELAY_PIN = 36
+
+def toggle_pump_relay():
+    relay = gpiozero.OutputDevice(RELAY_PIN)
+    relay.toggle()
+
+
+if __name__ == '__main__':
+    print('Starting the test script!')
+    while True:
+        sleep(2)
+        toggle_pump_relay()

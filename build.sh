b8c45a7cd4b062333b4e813c42240501c0224e21
erdeanmich <erdeanlinda@gmail.com>
2025-05-23 14:13:46 +0200

    use python 3

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ echo 'Building the backend'
 cd h2overlord-python
 poetry install 
 poetry lock
-poetry run python -m nuitka --mode=standalone --include-data-files=src/h2overlord_python/Config/config.json=Config/config.json src/h2overlord_python/main.py
+poetry run python3 -m nuitka --mode=standalone --include-data-files=src/h2overlord_python/Config/config.json=Config/config.json src/h2overlord_python/main.py
 cd ..
 mv h2overlord-python/main.dist build/backend
 
